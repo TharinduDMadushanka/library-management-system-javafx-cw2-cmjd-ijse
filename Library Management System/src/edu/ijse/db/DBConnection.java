5deeee8e5fa284fe4eda77f31ea4533db690c32b
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-10 14:10:54 +0530

    complete DBConnection class

--- Library Management System/src/edu/ijse/db/DBConnection.java
+++ Library Management System/src/edu/ijse/db/DBConnection.java
@@ -2,11 +2,33 @@ package edu.ijse.db;
 
 import edu.ijse.dto.Admin;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class DBConnection {
-    public static ArrayList<Admin> userTable = new ArrayList<>();
 
+    private static DBConnection dbConnection;
+    private Connection connection;
+
+    private DBConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","Thariya920@");
+    }
+
+    public static DBConnection getInstance() throws SQLException, ClassNotFoundException {
+        if (dbConnection == null) {
+            dbConnection = new DBConnection();
+        }
+        return dbConnection;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public static ArrayList<Admin> userTable = new ArrayList<>();
     static {
         userTable.add(new Admin(
                 "Tharindu",
