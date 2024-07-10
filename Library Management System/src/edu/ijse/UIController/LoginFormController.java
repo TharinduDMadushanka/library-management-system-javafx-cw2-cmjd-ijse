9878b6e6889653ac7d30ab0dba32112ee93959cc
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-10 08:40:12 +0530

    create Signup UI and genarate attributes & methods

--- Library Management System/src/edu/ijse/UIController/LoginFormController.java
+++ Library Management System/src/edu/ijse/UIController/LoginFormController.java
@@ -1,18 +1,48 @@
 package edu.ijse.UIController;
 
+import edu.ijse.db.DBConnection;
+import edu.ijse.dto.Admin;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Optional;
 
 public class LoginFormController {
     public TextField txtUserName;
     public PasswordField txtPassword;
     public Button btnLogin;
+    public Hyperlink signupLink;
 
-    public void loginOnAction(ActionEvent actionEvent) {
+    public void loginOnAction(ActionEvent actionEvent) throws IOException {
+        String userName = txtUserName.getText();
+        String password = txtPassword.getText();
+
+        Optional<Admin> selectAdmin = DBConnection.userTable.stream().filter(e -> e.getUsername().equals(userName)).findFirst();
+
+        if (selectAdmin.isPresent()) {
+            if(password.equals(selectAdmin.get().getPassword())) {
+                new Alert(Alert.AlertType.INFORMATION, "Login Successful").show();
+                setUI("MainForm");
+            }else {
+                new Alert(Alert.AlertType.ERROR, "Invalid Password").showAndWait();
+            }
+        }else {
+            new Alert(Alert.AlertType.ERROR, "Invalid User").showAndWait();
+        }
+
     }
 
     public void signupOnAction(ActionEvent actionEvent) {
+    }
+
+    private void setUI(String location) throws IOException {
+        Stage stage = (Stage) btnLogin.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/edu/ijse/view/" + location + ".fxml"))));
+        stage.show();
+        stage.centerOnScreen();
     }
 }
---
+++ Library Management System/src/edu/ijse/UIController/SignupFormController.java
@@ -0,0 +1,24 @@
+package edu.ijse.UIController;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class SignupFormController {
+    public AnchorPane signupContext;
+    public TextField txtName;
+    public PasswordField txtPassword;
+    public Button btnSignup;
+    public Hyperlink loginLink;
+    public TextField txtEmail;
+    public TextField txtUsername;
+
+    public void signupOnAction(ActionEvent actionEvent) {
+    }
+
+    public void LoginOnAction(ActionEvent actionEvent) {
+    }
+}
