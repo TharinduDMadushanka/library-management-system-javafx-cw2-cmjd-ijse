81b232ea739a977339dfc7125c09c84401c2be4f
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-10 08:10:56 +0530

    create Signup UI and genarate attributes & methods

--- Library Management System/src/Main.java
+++ Library Management System/src/Main.java
@@ -1,5 +1,18 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Library Management System");
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("edu/ijse/view/LoginForm.fxml"))));
+        stage.show();
+        stage.centerOnScreen();
     }
 }
---
+++ Library Management System/src/edu/ijse/UIController/LoginFormController.java
@@ -0,0 +1,18 @@
+package edu.ijse.UIController;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginFormController {
+    public TextField txtUserName;
+    public PasswordField txtPassword;
+    public Button btnLogin;
+
+    public void loginOnAction(ActionEvent actionEvent) {
+    }
+
+    public void signupOnAction(ActionEvent actionEvent) {
+    }
+}
