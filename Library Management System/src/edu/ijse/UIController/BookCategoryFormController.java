66477e158f85f6161796376050b4f536c008f11d
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-10 14:35:44 +0530

    create BookCategory UI form and genarate attributes & methods

---
+++ Library Management System/src/edu/ijse/UIController/BookCategoryFormController.java
@@ -0,0 +1,29 @@
+package edu.ijse.UIController;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class BookCategoryFormController {
+    public AnchorPane categoryContext;
+    public TextField txtId;
+    public TextField txtName;
+    public TableView categoryTable;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TextField txtSearch;
+
+    public void addOnAction(ActionEvent actionEvent) {
+    }
+
+    public void updateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void searchOnAction(ActionEvent actionEvent) {
+    }
+}
