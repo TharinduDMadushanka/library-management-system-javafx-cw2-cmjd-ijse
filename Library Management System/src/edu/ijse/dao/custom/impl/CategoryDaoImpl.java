f01f6d875fef5a8912513d0103902951efbbdd70
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-10 15:04:13 +0530

    complete CategoryDaoImpl class

--- Library Management System/src/edu/ijse/dao/custom/impl/CategoryDaoImpl.java
+++ Library Management System/src/edu/ijse/dao/custom/impl/CategoryDaoImpl.java
@@ -1,33 +1,52 @@
 package edu.ijse.dao.custom.impl;
 
+import edu.ijse.dao.CrudUtil;
 import edu.ijse.dao.custom.CategoryDao;
 import edu.ijse.entity.CategoryEntity;
 
+import java.sql.ResultSet;
 import java.util.ArrayList;
 
 public class CategoryDaoImpl implements CategoryDao {
     @Override
-    public boolean create(CategoryEntity categoryEntity) throws Exception {
-        return false;
+    public boolean create(CategoryEntity t) throws Exception {
+        return CrudUtil.executeUpdate("INSERT INTO category VALUES(?,?)",t.getId(),t.getName());
     }
 
     @Override
-    public boolean update(CategoryEntity categoryEntity) throws Exception {
-        return false;
+    public boolean update(CategoryEntity t) throws Exception {
+        return CrudUtil.executeUpdate("UPDATE category SET name=? WHERE id=?",t.getName(),t.getId());
     }
 
     @Override
-    public boolean delete(String s) throws Exception {
-        return false;
+    public boolean delete(String id) throws Exception {
+        return CrudUtil.executeUpdate("DELETE FROM category WHERE id=?",id);
     }
 
     @Override
-    public CategoryEntity get(String s) throws Exception {
+    public CategoryEntity get(String id) throws Exception {
+        ResultSet rst = CrudUtil.executeQuery("SELECT * FROM category WHERE id=?",id);
+        if(rst.next()){
+            CategoryEntity entity = new CategoryEntity(
+                    rst.getString("id"),
+                    rst.getString("name")
+            );
+            return entity;
+        }
         return null;
     }
 
     @Override
     public ArrayList<CategoryEntity> getAll() throws Exception {
-        return null;
+        ArrayList<CategoryEntity> categoryEntities = new ArrayList<>();
+        ResultSet rst = CrudUtil.executeQuery("SELECT * FROM category");
+        while(rst.next()){
+            CategoryEntity entity = new CategoryEntity(
+                    rst.getString("id"),
+                    rst.getString("name")
+            );
+            categoryEntities.add(entity);
+        }
+        return categoryEntities;
     }
 }
