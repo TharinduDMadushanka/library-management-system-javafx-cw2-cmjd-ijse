e052aea5ded098c125e714171b3652b4dfac83fd
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-11 09:59:36 +0530

    add BOOKS in to the ServiceFactory

--- Library Management System/src/edu/ijse/service/ServiceFactory.java
+++ Library Management System/src/edu/ijse/service/ServiceFactory.java
@@ -1,6 +1,7 @@
 package edu.ijse.service;
 
 import edu.ijse.service.custom.CategoryService;
+import edu.ijse.service.custom.impl.BookServiceImpl;
 import edu.ijse.service.custom.impl.CategoryServiceImpl;
 
 public class ServiceFactory {
@@ -19,12 +20,15 @@ public class ServiceFactory {
         switch (type) {
             case CATEGORY:
                 return new CategoryServiceImpl();
+            case BOOKS:
+                return new BookServiceImpl();
             default:
                 return null;
         }
     }
 
     public enum ServiceType {
-        CATEGORY
+        CATEGORY,
+        BOOKS
     }
 }
