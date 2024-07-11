ae82c743d2777adbb53731e150009408d08c95d1
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-11 09:06:16 +0530

    create BookForm UI and genarate attributes and methods

---
+++ Library Management System/src/edu/ijse/UIController/BooksFormController.java
@@ -0,0 +1,44 @@
+package edu.ijse.UIController;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class BooksFormController {
+    public AnchorPane bookContext;
+    public TextField txtBookId;
+    public TextField txtTitle;
+    public TextField txtAuthor;
+    public TextField txtCategoryShow;
+    public DatePicker txtYear;
+    public TextField txtSearch;
+    public TextField txtCategoryId;
+    public TableView bookTable;
+    public TableColumn colCategoryId;
+    public TableColumn colBookId;
+    public TableColumn colTitle;
+    public TableColumn colAuthor;
+    public TableColumn colYear;
+    public TableColumn colAvailable;
+
+    public void categorySearchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void addOnAction(ActionEvent actionEvent) {
+    }
+
+    public void updateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void clearOnAction(ActionEvent actionEvent) {
+    }
+
+    public void searchOnAction(ActionEvent actionEvent) {
+    }
+}
