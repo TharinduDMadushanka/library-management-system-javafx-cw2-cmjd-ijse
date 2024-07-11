25a435ae9c50c04c64e61c49173dd996b1a2e809
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-11 09:26:03 +0530

    complete BookEntity class

---
+++ Library Management System/src/edu/ijse/entity/BookEntity.java
@@ -0,0 +1,84 @@
+package edu.ijse.entity;
+
+import java.util.Date;
+
+public class BookEntity {
+    private String categoryId;
+    private String bookId;
+    private String title;
+    private String author;
+    private Date publishYear;
+    private String available;
+
+    public BookEntity() {
+    }
+
+    public BookEntity(String categoryId, String bookId, String title, String author, Date publishYear, String available) {
+        this.categoryId = categoryId;
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.publishYear = publishYear;
+        this.available = available;
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getPublishYear() {
+        return publishYear;
+    }
+
+    public void setPublishYear(Date publishYear) {
+        this.publishYear = publishYear;
+    }
+
+    public String getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(String available) {
+        this.available = available;
+    }
+
+    @Override
+    public String toString() {
+        return "BookEntity{" +
+                "categoryId='" + categoryId + '\'' +
+                ", bookId='" + bookId + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", publishYear=" + publishYear +
+                ", available='" + available + '\'' +
+                '}';
+    }
+}
