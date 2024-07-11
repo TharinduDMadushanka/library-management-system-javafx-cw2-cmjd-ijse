cfcab6784a94357b13390f71f8c1e822e05bfd26
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-11 09:50:53 +0530

    complete BookDaoImpl class

---
+++ Library Management System/src/edu/ijse/dao/custom/BookDao.java
@@ -0,0 +1,7 @@
+package edu.ijse.dao.custom;
+
+import edu.ijse.dao.CrudDao;
+import edu.ijse.entity.BookEntity;
+
+public interface BookDao extends CrudDao<BookEntity, String> {
+}
---
+++ Library Management System/src/edu/ijse/dao/custom/impl/BookDaoImpl.java
@@ -0,0 +1,62 @@
+package edu.ijse.dao.custom.impl;
+
+import edu.ijse.dao.CrudUtil;
+import edu.ijse.dao.custom.BookDao;
+import edu.ijse.entity.BookEntity;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class BookDaoImpl implements BookDao {
+    @Override
+    public boolean create(BookEntity t) throws Exception {
+        return CrudUtil.executeUpdate("INSERT INTO books VALUES(?,?,?,?,?,?)", t.getCategoryId(), t.getBookId(), t.getTitle(),
+                t.getAuthor(), t.getPublishYear(), t.getAvailable());
+    }
+
+    @Override
+    public boolean update(BookEntity t) throws Exception {
+        return CrudUtil.executeUpdate("UPDATE books SET categoryId = ?,title = ?,author=?,publishYear=?,available=? WHERE bookId=?",
+                t.getCategoryId(), t.getTitle(), t.getAuthor(), t.getPublishYear(), t.getAvailable(), t.getBookId());
+    }
+
+    @Override
+    public boolean delete(String id) throws Exception {
+        return CrudUtil.executeUpdate("DELETE FROM books WHERE bookId=?", id);
+    }
+
+    @Override
+    public BookEntity get(String id) throws Exception {
+        ResultSet rst = CrudUtil.executeQuery("SELECT * FROM books WHERE bookId=?", id);
+        if (rst.next()) {
+            BookEntity entity = new BookEntity(
+                    rst.getString("bookId"),
+                    rst.getString("categoryId"),
+                    rst.getString("title"),
+                    rst.getString("author"),
+                    rst.getDate("publishYear"),
+                    rst.getString("available")
+            );
+            return entity;
+        }
+        return null;
+    }
+
+    @Override
+    public ArrayList<BookEntity> getAll() throws Exception {
+        ArrayList<BookEntity> bookEntities = new ArrayList<>();
+        ResultSet rst = CrudUtil.executeQuery("SELECT * FROM books");
+        while (rst.next()) {
+            BookEntity entity = new BookEntity(
+                    rst.getString("bookId"),
+                    rst.getString("categoryId"),
+                    rst.getString("title"),
+                    rst.getString("author"),
+                    rst.getDate("publishYear"),
+                    rst.getString("available")
+            );
+            bookEntities.add(entity);
+        }
+        return bookEntities;
+    }
+}
