c03d9a2b8a7a3211dd44fd8671f2b7e145f3bbb6
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-12 11:37:18 +0530

    complete operations realted to Member Management

--- Library Management System/src/edu/ijse/UIController/MemberFormController.java
+++ Library Management System/src/edu/ijse/UIController/MemberFormController.java
@@ -28,15 +28,14 @@ public class MemberFormController {
     public TextField txtSearch;
 
     public TableView<MemberDto> memberTable;
-    public TableColumn<MemberDto,String> colId;
-    public TableColumn<MemberDto,String> colName;
-    public TableColumn<MemberDto,String> colAddress;
-    public TableColumn<MemberDto,String> colMobile;
-    public TableColumn<MemberDto,String> colEmail;
-    public TableColumn<MemberDto,String> colDob;
-    public TableColumn<MemberDto,String> colAge;
-    public TableColumn<MemberDto,String> colGender;
-
+    public TableColumn<MemberDto, String> colId;
+    public TableColumn<MemberDto, String> colName;
+    public TableColumn<MemberDto, String> colAddress;
+    public TableColumn<MemberDto, String> colMobile;
+    public TableColumn<MemberDto, String> colEmail;
+    public TableColumn<MemberDto, String> colDob;
+    public TableColumn<MemberDto, String> colAge;
+    public TableColumn<MemberDto, String> colGender;
 
 
     private MemberService memberService = new MemberServiceImpl();
@@ -86,17 +85,17 @@ public class MemberFormController {
         try {
 
             String result = memberService.save(member);
-            if("Success".equals(result)) {
+            if ("Success".equals(result)) {
                 memberList.add(member);
                 memberTable.refresh();
-                new Alert(Alert.AlertType.INFORMATION,"Member Added Successfully").show();
+                new Alert(Alert.AlertType.INFORMATION, "Member Added Successfully").show();
                 clearFields();
                 setMemberId();
             }
 
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-            new Alert(Alert.AlertType.INFORMATION,"Member save failed..!").show();
+            new Alert(Alert.AlertType.INFORMATION, "Member save failed..!").show();
         }
 
     }
@@ -118,16 +117,16 @@ public class MemberFormController {
         try {
 
             String result = memberService.update(member);
-            if("Success".equals(result)) {
+            if ("Success".equals(result)) {
                 loadMember();
-                new Alert(Alert.AlertType.INFORMATION,"Member Updated Successfully").show();
+                new Alert(Alert.AlertType.INFORMATION, "Member Updated Successfully").show();
                 clearFields();
                 setMemberId();
             }
 
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-            new Alert(Alert.AlertType.INFORMATION,"Member update failed..!").show();
+            new Alert(Alert.AlertType.INFORMATION, "Member update failed..!").show();
         }
 
     }
@@ -138,15 +137,15 @@ public class MemberFormController {
         try {
 
             String result = memberService.delete(id);
-            if("Success".equals(result)) {
+            if ("Success".equals(result)) {
                 loadMember();
-                new Alert(Alert.AlertType.INFORMATION,"Member Deleted Successfully").show();
+                new Alert(Alert.AlertType.INFORMATION, "Member Deleted Successfully").show();
                 clearFields();
             }
 
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-            new Alert(Alert.AlertType.INFORMATION,"Member delete failed..!").show();
+            new Alert(Alert.AlertType.INFORMATION, "Member delete failed..!").show();
         }
 
     }
@@ -157,42 +156,42 @@ public class MemberFormController {
 
     public void searchOnAction(ActionEvent actionEvent) {
 
-        try{
+        try {
 
             String memberId = txtSearch.getText().trim();
 
-            if (!memberId.isEmpty()){
+            if (!memberId.isEmpty()) {
                 MemberDto member = memberService.get(memberId);
-                if (member != null){
+                if (member != null) {
                     memberList.clear();
                     memberList.add(member);
                     memberTable.setItems(memberList);
-                }else {
-                    new Alert(Alert.AlertType.INFORMATION,"No member found with ID "+memberId).show();
+                } else {
+                    new Alert(Alert.AlertType.INFORMATION, "No member found with ID " + memberId).show();
                     loadMember();
                 }
-            }else {
-                new Alert(Alert.AlertType.INFORMATION,"Please enter a Member ID to search").show();
+            } else {
+                new Alert(Alert.AlertType.INFORMATION, "Please enter a Member ID to search").show();
                 loadMember();
             }
 
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-            new Alert(Alert.AlertType.INFORMATION,"Member search failed..!").show();
+            new Alert(Alert.AlertType.INFORMATION, "Member search failed..!").show();
         }
 
     }
 
-    private void loadMember(){
+    private void loadMember() {
         try {
 
             ArrayList<MemberDto> members = memberService.getAll();
-            if(members != null){
+            if (members != null) {
                 memberList.setAll(members);
                 memberTable.setItems(memberList);
             }
 
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -208,9 +207,9 @@ public class MemberFormController {
         if (txtGender != null) txtGender.clear();
     }
 
-    private void selectMember(MouseEvent mouseEvent){
+    private void selectMember(MouseEvent mouseEvent) {
         MemberDto selectedMember = memberTable.getSelectionModel().getSelectedItem();
-        if(selectedMember != null){
+        if (selectedMember != null) {
             txtId.setText(selectedMember.getMemberId());
             txtName.setText(selectedMember.getMemberName());
             txtAddress.setText(selectedMember.getAddress());
@@ -221,7 +220,8 @@ public class MemberFormController {
             cmbGender.getSelectionModel().select(selectedMember.getGender());
         }
     }
-    private void setMemberId(){
+
+    private void setMemberId() {
         String memberId = "M-";
         txtId.setText(memberId);
     }
