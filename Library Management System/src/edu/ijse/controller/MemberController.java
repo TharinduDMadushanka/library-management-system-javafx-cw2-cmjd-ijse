62224e272b36d0c6c931f0aa0c371fcc45bd75ce
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-12 09:33:55 +0530

    complete MemberController class

--- Library Management System/src/edu/ijse/controller/MemberController.java
+++ Library Management System/src/edu/ijse/controller/MemberController.java
@@ -1,4 +1,31 @@
 package edu.ijse.controller;
 
+import edu.ijse.dto.MemberDto;
+import edu.ijse.service.ServiceFactory;
+import edu.ijse.service.custom.MemberService;
+
+import java.util.ArrayList;
+
 public class MemberController {
+    private MemberService memberService = (MemberService) ServiceFactory.getInstance().getService(ServiceFactory.ServiceType.MEMBERS);
+
+    public String save(MemberDto memberDto)throws Exception{
+        return memberService.save(memberDto);
+    }
+
+    public String update(MemberDto memberDto)throws Exception{
+        return memberService.update(memberDto);
+    }
+
+    public String delete(String memberId)throws Exception{
+        return memberService.delete(memberId);
+    }
+
+    public ArrayList<MemberDto> getAll()throws Exception{
+        return memberService.getAll();
+    }
+
+    public MemberDto getMember(String memberId)throws Exception{
+        return memberService.get(memberId);
+    }
 }
