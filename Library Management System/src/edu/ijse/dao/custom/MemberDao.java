86f63f79f36943b1142e85815c55446254a3282a
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-12 09:13:34 +0530

    complete MemberDaoImpl class

---
+++ Library Management System/src/edu/ijse/dao/custom/MemberDao.java
@@ -0,0 +1,7 @@
+package edu.ijse.dao.custom;
+
+import edu.ijse.dao.CrudDao;
+import edu.ijse.entity.MemberEntity;
+
+public interface MemberDao extends CrudDao<MemberEntity,String> {
+}
---
+++ Library Management System/src/edu/ijse/dao/custom/impl/MemberDaoImpl.java
@@ -0,0 +1,67 @@
+package edu.ijse.dao.custom.impl;
+
+import edu.ijse.dao.CrudUtil;
+import edu.ijse.dao.custom.MemberDao;
+import edu.ijse.entity.MemberEntity;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class MemberDaoImpl implements MemberDao {
+    @Override
+    public boolean create(MemberEntity t) throws Exception {
+        return CrudUtil.executeUpdate("INSERT INTO members VALUES(?,?,?,?,?,?,?,?) ",t.getMemberId(),t.getMemberName(),t.getAddress(),
+                t.getMobile(),t.getEmail(),t.getAge(),t.getDob(),t.getGender());
+    }
+
+    @Override
+    public boolean update(MemberEntity t) throws Exception {
+        return CrudUtil.executeUpdate("UPDATE members SET memberName=?,address=?,mobileNumber=?,email=?,age=?,dob=?,gender=? WHERE memberId=?",
+                t.getMemberName(),t.getAddress(), t.getMobile(),t.getEmail(),t.getAge(),t.getDob(),t.getGender(),t.getMemberId());
+    }
+
+    @Override
+    public boolean delete(String id) throws Exception {
+        return CrudUtil.executeUpdate("DELETE FROM members WHERE memberId=?",id);
+    }
+
+    @Override
+    public MemberEntity get(String id) throws Exception {
+        ResultSet rst = CrudUtil.executeQuery("SELECT * FROM members WHERE memberId=?",id);
+        if(rst.next()){
+            MemberEntity memberEntity = new MemberEntity(
+                    rst.getString("memberId"),
+                    rst.getString("memberName"),
+                    rst.getString("address"),
+                    rst.getString("mobileNumber"),
+                    rst.getString("email"),
+                    rst.getInt("age"),
+                    rst.getDate("dob").toLocalDate(),
+                    rst.getString("gender")
+            );
+            return memberEntity;
+        }
+        return null;
+    }
+
+    @Override
+    public ArrayList<MemberEntity> getAll() throws Exception {
+        ArrayList<MemberEntity> memberEntities = new ArrayList<>();
+        ResultSet rst = CrudUtil.executeQuery("SELECT * FROM members");
+
+        while(rst.next()){
+            MemberEntity memberEntity = new MemberEntity(
+                    rst.getString("memberId"),
+                    rst.getString("memberName"),
+                    rst.getString("address"),
+                    rst.getString("mobileNumber"),
+                    rst.getString("email"),
+                    rst.getInt("age"),
+                    rst.getDate("dob").toLocalDate(),
+                    rst.getString("gender")
+            );
+            memberEntities.add(memberEntity);
+        }
+        return memberEntities;
+    }
+}
