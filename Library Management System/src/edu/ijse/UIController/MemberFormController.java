9c896eb0c635af39c1ae15790b74410ab3c70da3
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-12 08:09:14 +0530

    create Member UI and genarate all attributes and methods

---
+++ Library Management System/src/edu/ijse/UIController/MemberFormController.java
@@ -0,0 +1,41 @@
+package edu.ijse.UIController;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+
+public class MemberFormController {
+    public AnchorPane memberContext;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField txtMobile;
+    public TextField txtEmail;
+    public TextField txtAge;
+    public DatePicker txtDob;
+    public ComboBox txtGender;
+    public TextField txtId;
+    public TableView memberTable;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colAddress;
+    public TableColumn colMobile;
+    public TableColumn colEmail;
+    public TableColumn colDob;
+    public TableColumn colAge;
+    public TableColumn colGender;
+
+    public void addOnAction(ActionEvent actionEvent) {
+    }
+
+    public void updateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void clearOnAction(ActionEvent actionEvent) {
+    }
+
+    public void searchOnAction(ActionEvent actionEvent) {
+    }
+}
