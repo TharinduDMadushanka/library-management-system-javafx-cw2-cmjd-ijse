6a418cf6937ac56c2cc19e45f5bd126f7c5c1ac6
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-16 07:51:00 +0530

    create ReturnBookForm UI and genarate alll attributes a

---
+++ Library Management System/src/edu/ijse/UIController/ReturnBookFormController.java
@@ -0,0 +1,36 @@
+package edu.ijse.UIController;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class ReturnBookFormController {
+    public AnchorPane returnBookContext;
+    public TextField txtIssueDate;
+    public TextField txtDueDate;
+    public TextField txtMemId;
+    public TextField txtName;
+    public TextField txtAge;
+    public TextField txtAddress;
+    public TextField txtEmail;
+    public TextField txtMobile;
+    public TextField txtGender;
+    public TextField txtDob;
+    public TextField txtIssueId;
+    public TextField txtBookDetails;
+    public TextField txtBookId;
+    public TextField txtMemberId;
+    public TextField txtMemberDetails;
+    public DatePicker txtReturnDate;
+    public TextField txtFine;
+
+    public void searchIssueBookOnAction(ActionEvent actionEvent) {
+    }
+
+    public void searchMemberOnAction(ActionEvent actionEvent) {
+    }
+
+    public void returnBookOnAction(ActionEvent actionEvent) {
+    }
+}
