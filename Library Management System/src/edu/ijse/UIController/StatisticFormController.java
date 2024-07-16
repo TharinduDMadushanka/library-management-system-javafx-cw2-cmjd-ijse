108e49dc9163debcc9f7650ea192c337be62c8a2
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-16 08:44:29 +0530

    create StatisticForm UI and genarate all attributes

---
+++ Library Management System/src/edu/ijse/UIController/StatisticFormController.java
@@ -0,0 +1,27 @@
+package edu.ijse.UIController;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
+public class StatisticFormController {
+    public AnchorPane statisticContext;
+    public TableView statisticTable;
+    public TableColumn colId;
+    public TableColumn colIssueID;
+    public TableColumn colBookId;
+    public TableColumn colBookDetail;
+    public TableColumn colMemberId;
+    public TableColumn colMemberDetails;
+    public TableColumn colIssueId;
+    public TableColumn colReturnDate;
+    public TableColumn colDueDate;
+    public TableColumn colFine;
+
+    public void returnBookOnAction(ActionEvent actionEvent) {
+    }
+
+    public void dashboardOnAction(ActionEvent actionEvent) {
+    }
+}
